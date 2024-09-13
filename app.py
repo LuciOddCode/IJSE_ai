d2a2d5fd259713bc919d85f3bea4ea73f367a1ef
LuciOddCode <dasanayakarusiru@gmail.com>
2024-09-13 21:41:31 +0530

    new push 4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>No Kidding Im the Best V7</h1>'
+    return '<h1>No Kidding Im the Best V8</h1>'
 
 if __name__ == '__main__':
     #app.run(debug=True)
